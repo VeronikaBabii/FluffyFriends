a3a641eba5aee3012e6fab096356709ef5ea79e1
VeronikaBabii <veronika.babii.2019@gmail.com>
2020-05-05 14:01:44 +0300

    Initial Commit

---
+++ FluffyFriends/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FluffyFriends
+//
+//  Created by Veronika Babii on 05.05.2020.
+//  Copyright © 2020 Veronika Babii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
